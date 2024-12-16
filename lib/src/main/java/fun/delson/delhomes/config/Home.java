3fecc9183fef2dbbd5855c2b3838e69b409a6636
DeLsonJabberwo <delsonl@proton.me>
2024-12-16 16:47:39 -0600

    feat: set up json config interaction

--- lib/src/main/java/fun/delson/delhomes/config/Home.java
+++ lib/src/main/java/fun/delson/delhomes/config/Home.java
@@ -2,9 +2,22 @@ package fun.delson.delhomes.config;
 
 import java.util.UUID;
 
+import org.bukkit.Location;
+
 public class Home {
     public String name;
     public UUID world;
     public String worldName;
-    public float x, y, z, yaw, pitch;
+    public double x, y, z, yaw, pitch;
+
+    public Home(String name, Location location) {
+        this.name = name;
+        this.world = location.getWorld().getUID();
+        this.worldName = location.getWorld().getName();
+        this.x = location.getX();
+        this.y = location.getY();
+        this.z = location.getZ();
+        this.yaw = location.getYaw();
+        this.pitch = location.getPitch();
+    }
 }
