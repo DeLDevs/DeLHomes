2adcbf3d34f47214f6a6d1396e8b3a0c0bf8b180
DeLsonJabberwo <pdhl2006@gmail.com>
2024-12-17 17:34:37 -0600

    feat: /homes command + join/leave clean-up

---
+++ lib/src/main/java/fun/delson/delhomes/commands/Homes.java
@@ -0,0 +1,46 @@
+package fun.delson.delhomes.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import fun.delson.delhomes.config.PlayerConfig;
+import fun.delson.delhomes.utils.Chat;
+import fun.delson.delhomes.utils.PlayerConfigUtils;
+
+public class Homes implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        Player player = (Player) sender;
+
+        if (sender instanceof Player) {
+
+            PlayerConfig config = PlayerConfigUtils.getPlayerConfig(player);
+            if (config == null) {
+                Bukkit.getLogger().info(Chat.color("&cPlayer &6" + player.getName() + "&c not found."));
+                player.sendMessage(Chat.color("&cPlayer &6" + player.getName() + "&c not found."));
+                return false;
+            }
+            String response = "&6Homes: &r";
+            response += config.homes[0].name;
+            for (int i = 1; i < config.homes.length; i++) {
+                response += ", " + config.homes[i].name;
+            }
+            player.sendMessage(Chat.color(response));
+
+        } else {
+            sender.sendMessage(Chat.color("&6You must be a player to use this command."));
+            return false;
+        }
+
+        return true;
+
+    }
+
+}
+
