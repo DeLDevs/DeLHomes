1b4deb4036409b412654d978b3bac293b14fc89b
DeLsonJabberwo <pdhl2006@gmail.com>
2024-12-17 18:44:35 -0600

    fix: limited /back suggestion to death respawn

--- lib/src/main/java/fun/delson/delhomes/listeners/PlayerRespawn.java
+++ lib/src/main/java/fun/delson/delhomes/listeners/PlayerRespawn.java
@@ -1,14 +1,12 @@
 package fun.delson.delhomes.listeners;
 
-import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerRespawnEvent.RespawnReason;
 
-import fun.delson.delhomes.config.PlayerConfig;
 import fun.delson.delhomes.utils.Chat;
-import fun.delson.delhomes.utils.PlayerConfigUtils;
 
 public class PlayerRespawn implements Listener {
 
@@ -17,7 +15,9 @@ public class PlayerRespawn implements Listener {
 
         Player player = event.getPlayer();
 
-        player.sendMessage(Chat.color("&6Type &c/back &6 to return to the location of your death."));
+        if (event.getRespawnReason().equals(RespawnReason.DEATH)) {
+            player.sendMessage(Chat.color("&6Type &c/back &6 to return to the location of your death."));
+        }
 
     }
 
