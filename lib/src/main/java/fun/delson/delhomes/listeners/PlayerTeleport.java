b865eb17474669713745494573c84d978be090b9
DeLsonJabberwo <pdhl2006@gmail.com>
2024-12-23 22:13:43 -0600

    fix: limited /back to plugin and command teleports

--- lib/src/main/java/fun/delson/delhomes/listeners/PlayerTeleport.java
+++ lib/src/main/java/fun/delson/delhomes/listeners/PlayerTeleport.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerTeleportEvent.TeleportCause;
 
 import fun.delson.delhomes.config.PlayerConfig;
 import fun.delson.delhomes.utils.PlayerConfigUtils;
@@ -17,7 +18,16 @@ public class PlayerTeleport implements Listener {
         Player player = event.getPlayer();
         PlayerConfig config = PlayerConfigUtils.getPlayerConfig(player);
         Location fromLoc = event.getFrom();
-        config.setBack(fromLoc);
+        TeleportCause[] allowedReasons = { TeleportCause.COMMAND, TeleportCause.PLUGIN };
+        boolean allowed = false;
+        for (TeleportCause t : allowedReasons) {
+            if (event.getCause().equals(t)) {
+                allowed = true;
+            }
+        }
+        if (allowed) {
+            config.setBack(fromLoc);
+        }
 
     }
 
